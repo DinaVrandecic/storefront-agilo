00dbc1bf022b84a0a3dd892a6e84c1c536ee711f
DinaVrandecic <dina.vrandecic@gmail.com>
2024-06-21 14:53:26 +0200

    initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hamburger from "./Hamburger";
+
+export default function Header() {
+  return (
+    <div className="">
+      <Hamburger></Hamburger>
+      <p>hell01</p>
+    </div>
+  );
+}
