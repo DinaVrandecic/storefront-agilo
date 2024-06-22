17d96e9017ff5fd5210c20bf3e5dee5d4cad6e9e
DinaVrandecic <dina.vrandecic@gmail.com>
2024-06-22 19:32:07 +0200

    Add a function that finds all categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
 import products from "../public/data.json";
+import { BiCategory } from "react-icons/bi";
 
 export default function Home() {
+  const uniqueCategories = Array.from(
+    new Set(products.map((product) => product.category))
+  );
+
   return (
     <main className="">
       <Header />
-      <div className="flex justify-center my-10">
+      {uniqueCategories.map((category) => (
+        <li key={category} className="cursor-pointer">
+          {category}
+        </li>
+      ))}
+      <div className="flex justify-center mt-10">
         <section className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {products.map((product) => (
             <Card
