6414edb85911f40fcac3d343d3f20e1cfe65fa3b
DinaVrandecic <dina.vrandecic@gmail.com>
2024-06-22 16:46:51 +0200

    Add data into grid from json file

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,22 +1,58 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import BlueShirt from "../pictures/T-Shirt-blue.webp";
+// import React from "react";
+// import Link from "next/link";
+// import Image from "next/image";
+// import BlueShirt from "../pictures/T-Shirt-blue.webp";
 
-export default function Card() {
+// export default function Card() {
+//   return (
+//     <div className="flex flex-col items-center bg-lightgray shadow w-fit h-fit pb-[20px] m-[10px] hover:bg-gray hover:cursor-pointer">
+//       <div className="m-[20px]">
+//         <Image
+//           src={BlueShirt}
+//           alt="Logo T-Shirt"
+//           width={300}
+//           height={300}
+//           style={{ width: "300px", height: "300px" }}
+//         />
+//       </div>
+//       <p className="text-xl font-bold text-black">Logo T-Shirt</p>
+//       <p className="text-xl text-black">30€</p>
+//     </div>
+//   );
+// }
+
+// components/Card.tsx
+import React from "react";
+import Image from "next/image";
+
+interface CardProps {
+  name: string;
+  price: string;
+  description: string;
+  material: string;
+  image: string;
+}
+
+export default function Card({
+  name,
+  price,
+  description,
+  material,
+  image,
+}: CardProps) {
   return (
     <div className="flex flex-col items-center bg-lightgray shadow w-fit h-fit pb-[20px] m-[10px] hover:bg-gray hover:cursor-pointer">
       <div className="m-[20px]">
         <Image
-          src={BlueShirt}
-          alt="Logo T-Shirt"
+          src={image}
+          alt={name}
           width={300}
           height={300}
           style={{ width: "300px", height: "300px" }}
         />
       </div>
-      <p className="text-xl font-bold text-black">Logo T-Shirt</p>
-      <p className="text-xl text-black">30€</p>
+      <p className="text-xl font-bold text-black">{name}</p>
+      <p className="text-xl text-black">{price}</p>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
+import products from "../public/data.json";
 
 export default function Home() {
   return (
     <main className="">
-      <Header></Header>
+      <Header />
       <div className="flex justify-center mt-10">
         <section className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              description={product.description}
+              material={product.material}
+              image={product.variants[0].images[0]}
+            />
+          ))}
         </section>
       </div>
     </main>
